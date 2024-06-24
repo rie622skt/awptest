3707834373f251508c79a915b5b015b6dd125f7f
riE622skt <147387050+riE622skt@users.noreply.github.com>
2024-06-24 10:42:18 +0900

    Update index.js

--- index.js
+++ index.js
@@ -8,3 +8,30 @@ if ('serviceWorker' in navigator) {
       });
     });
   }
+  
+  let deferredPrompt;
+
+  window.addEventListener('beforeinstallprompt', (e) => {
+    // インストールプロンプトを抑制し、後で表示できるように保存
+    e.preventDefault();
+    deferredPrompt = e;
+    // インストールボタンを表示する
+    const installButton = document.createElement('button');
+    installButton.innerText = 'Install PWA';
+    document.body.appendChild(installButton);
+  
+    installButton.addEventListener('click', () => {
+      // プロンプトを表示する
+      deferredPrompt.prompt();
+      // ユーザーが選択した結果を処理する
+      deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        deferredPrompt = null;
+      });
+    });
+  });
+  
